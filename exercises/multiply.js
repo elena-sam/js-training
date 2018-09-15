8d3b87be72a0e52ecbfee4f88332385acf630514
Emilie <hemilie94@gmail.com>
2018-09-15 17:34:51 +0200

    resolve issue in multiply.js

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -16,7 +16,7 @@ function multiply(n, m) {
   for (let i=0;i<x;i++) {
     total += y;
   }
-  return (n>=0&&m>=0 || n<=-1&&m<=-1)?total:-total;
+  return (n>0&&m>0 || n<=0&&m<=0)?total:-total;
 }
 //* Begin of tests
 const assert = require('assert')
